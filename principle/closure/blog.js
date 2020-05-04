c7f40c2fd9cc9db3f986c43cd38f3621bdf28a2e
sjk5766 <wjdrnr6662@gmail.com>
2020-05-04 19:02:20 +0900

    closure 완료

--- principle/closure/blog.js
+++ principle/closure/blog.js
@@ -1,3 +1,4 @@
+// Simple Closure Ex
 var x = 1;
 
 function outer(z) {
@@ -10,7 +11,7 @@ function outer(z) {
 var sum = outer(3);
 sum();
 
-
+// Closure private 
 var count = (function () {
     var _count = 0;
     function changeVal(val) {
@@ -34,3 +35,37 @@ count.inc()
 count.inc()
 count.inc()
 console.log(count.get()); // 3
+
+// Scope Chain Ex
+var x = 1;
+
+function outer(z) {
+    var y = 2;
+    return function middle(p) { 
+        var m = 4;
+        return function inner() {
+            console.log(x + y + z + m + p) 
+        }
+    }
+}
+
+var middle = outer(3);
+var inner = middle(5);
+console.dir(inner)
+
+// Scope Chain Ex 2
+var x = 1;
+
+function outer(z) {
+    var y = 2;
+    return function middle(p) { 
+        var m = 4;
+        return function inner() {
+            console.log(x + y + z) // inner에서 middle 함수의 변수와 인자를 사용 x
+        }
+    }
+}
+
+var middle = outer(3);
+var inner = middle(5);
+console.dir(inner)
