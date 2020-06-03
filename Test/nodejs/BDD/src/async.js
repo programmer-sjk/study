4005f70c3fd90e43661c02253dd902beca9f608c
sjk5766 <wjdrnr6662@gmail.com>
2020-06-03 12:40:04 +0900

    비동기 axios 테스트 추가

--- Test/nodejs/BDD/src/async.js
+++ Test/nodejs/BDD/src/async.js
@@ -11,3 +11,18 @@ exports.fun_promise = () => {
         }, 100)
     })
 }
+
+const axios = require('axios');
+exports.fun_axios = (cb) => {
+    axios.get('https://reqres.in/api/products/3')   
+    .then(r => cb && cb(r.data))
+    .catch(e => cb(e))
+}
+
+exports.fun_axios2 = () => {
+    return new Promise((resolve, reject) => {
+        axios.get('https://reqres.in/api/products/3')   
+        .then( r => resolve(r.data))
+        .catch(e => reject(e))
+    })
+}
--- Test/nodejs/BDD/test/async-test.js
+++ Test/nodejs/BDD/test/async-test.js
@@ -1,6 +1,8 @@
 var assert = require('assert');
 const fun_async = require('../src/async').fun_async
 const fun_promise = require('../src/async').fun_promise
+const fun_axios =  require('../src/async').fun_axios
+const fun_axios2 =  require('../src/async').fun_axios2
 
 describe('Async Test', () => {
     it('setTimeout 100ms callback test', (done) => {
@@ -37,10 +39,30 @@ describe('Async Test', () => {
     it('async await test', async () => {
         const ret = await fun_promise()
         try {
-            assert.equal(ret, 2)
+            assert.equal(ret, 1)
         } catch(e) {
             console.log(e)
         }
         
+    })
+
+    it('axios test', (done) => {
+        fun_axios( r => {
+            try {
+                assert.equal(r['data']['id'], 3)
+                done()
+            } catch(e) {
+                console.log(e)
+            }
+        })
+    })
+
+    it('axios test2', (done) => {
+        fun_axios2()
+        .then(r => {
+            assert.equal(r['data']['id'], 3)
+            done()
+        })
+        .catch(done)
     })
 })
