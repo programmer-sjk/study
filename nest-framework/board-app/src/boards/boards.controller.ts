12737582e01ca8e927493fb94c30d0100b4ae240
ggugi <sjk5766@inflearn.com>
2021-08-28 16:50:24 +0900

    nest js 실습

---
+++ nest-framework/board-app/src/boards/boards.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, NotFoundException, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardService: BoardsService) {}
+
+  @Get()
+  getAllBoard(): Board[] {
+    return this.boardService.getAllBoards();
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string) {
+    const board = this.boardService.getBoardById(id);
+    if (!board) {
+      throw new NotFoundException();
+    }
+  }
+}
