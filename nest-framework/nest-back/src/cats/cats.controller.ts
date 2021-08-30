a3314ec08859e88cb18c41bdd8ef7b50af8b4cd4
ggugi <sjk5766@inflearn.com>
2021-08-31 01:03:44 +0900

    nest js 실습

---
+++ nest-framework/nest-back/src/cats/cats.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Patch, Post, Put } from '@nestjs/common';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getAllCat() {
+    return 'all cat';
+  }
+
+  @Get(':id')
+  getOneCat() {
+    return 'one cat';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updateParticalCat() {
+    return 'update';
+  }
+
+}
