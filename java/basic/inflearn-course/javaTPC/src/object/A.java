576a4f60204d2053c481f70e9d9ca064ef3d849f
seojeongkuk <wjdrnr6662@gmail.com>
2022-02-15 10:33:52 +0900

    object 실습

---
+++ java/basic/inflearn-course/javaTPC/src/object/A.java
@@ -0,0 +1,15 @@
+package object;
+
+public class A extends Object{
+    public A() {
+        super();
+    }
+
+    public void display() {
+        System.out.println("나는 A이다");
+    }
+
+//    public String toString() {
+//        return "A가 재 정의한 메서드 입니다.";
+//    }
+}
---
+++ java/basic/inflearn-course/javaTPC/src/object/Main.java
@@ -0,0 +1,10 @@
+package object;
+
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+        a.display();
+        System.out.println(a.toString());
+        System.out.println(a);
+    }
+}
