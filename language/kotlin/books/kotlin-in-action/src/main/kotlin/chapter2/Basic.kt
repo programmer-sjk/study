1e76f83960b2cd13e3e4bd4752a67e4e3a2aab86
seojeongkuk <wjdrnr6662@gmail.com>
2022-06-08 23:23:34 +0900

    [kotlin-in-action] 2장 실습중 - 기초 및 커스텀 연산자

--- language/kotlin/books/kotlin-in-action/src/main/kotlin/chapter2/Basic.kt
+++ language/kotlin/books/kotlin-in-action/src/main/kotlin/chapter2/Basic.kt
@@ -2,4 +2,15 @@ package chapter2
 
 fun main() {
     println("hello")
+    println(max(5, 3))
+}
+
+fun max(a: Int, b: Int) = if (a > b) a else b
+
+/** 커스텀 접근자*/
+class Rectangle(val height: Int, val width: Int) {
+    val isSquare: Boolean
+        get() {
+            return height == width
+        }
 }
